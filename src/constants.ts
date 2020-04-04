0710ed31ddf1a2dae1298686385c043d4e026c4b
Anaïs Betts <anais@anaisbetts.org>
2020-04-04 15:58:57 -0700

    y even

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 module.exports = {
   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-  webHookBase: 'https://hooks.slack.com/services/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',
+  webHookBase: 'https://hooks.slack.com/services/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',
 }
